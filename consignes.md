fd7cf6cda37eb59e6f96cafe1dfdda7f7130ee75
Hilal Koujil <hilal1993@hotmail.be>
2021-10-06 10:37:49 +0200

    fini

---
+++ consignes.md
@@ -0,0 +1,19 @@
+# Exercice 1 
+- Déclarez une variable qui reçoit 1 chiffre
+- Déclarez une variable qui variable reçoit 1 autre chiffre
+- Afficher l'addition, soustraction, division et multiplication entre les 2 chiffres
+    - exemple: ` variable1 + variable2 = resultat `
+
+# Exercice 2 
+
+- Demander via un prompt l'age de l'utilisateur
+- Demander via un prompt le prénom de l'utilisateur'
+- Afficher dans la console le message : "prenom a age ans cette année"
+- Afficher dans la console le message : "prenom aura age ans dans 3 ans" (attention avec l'age de la personne)
+
+# Exercice 3 
+
+- Créez une variable avec l'année de votre naissance
+- Créez une variable avec l'année actuelle
+- Affichez votre age à l'année actuelle
+- Affichez votre age 2 ans aprés l'année actuelle
---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+let monNombre=50;
+let monNombre2=60;
+console.log(`${monNombre} + ${monNombre2} = ${monNombre+monNombre2}`);
+console.log(`${monNombre} + ${monNombre2} = ${monNombre-monNombre2}`);
+console.log(`${monNombre} + ${monNombre2} = ${monNombre*monNombre2}`);
+console.log(`${monNombre} + ${monNombre2} = ${monNombre/monNombre2}`);
+//
+
+let chiffre = prompt("ercrire un chiffre:");
+let chiffre2 = prompt("ercrire un chiffre:");
+let a=parseInt(chiffre);
+let b=parseInt(chiffre2);
+
+let result = `${a + b}`;
+console.log(result);
+
+//
+
+let age = prompt("ercrivez votre age:");
+let ages=parseInt(age);
+let nom = prompt("quel est votre nom:");
+
+console.log("vous avez " + ages);
+console.log(`${nom} aura ${ages+3} ans dans 3 ans`);
+
+
+//
+
+let naissance=1993;
+let anneeActuelle =2021;
+
+console.log(anneeActuelle-naissance);
+
+anneeActuelle=ages; 
+console.log(anneeActuelle);
+console.log(anneeActuelle+2);
+//fin
